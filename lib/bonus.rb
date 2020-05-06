23a868dc0ee12cbd491724fa5f9353446f4fd223
Toni  Diep <adiep123@gmail.com>
2020-05-06 01:32:05 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -26,7 +26,8 @@ def bonus
   
   epic_tragedy[:capulet][:heroine][:status] = "dead"
   
-
+    puts epic_tragedy
+    
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   
 end
